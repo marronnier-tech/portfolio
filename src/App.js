94a1530bc5c4a9486eada03340d3f503a533f922
Reina Tochigi <tocchy@yutakanoMacBook-Pro.local>
2020-12-14 11:28:37 +0900

    必要なページを用意し、ルーティングを作った

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg˚
 import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Title from './components/Title';
+import Works from './components/Works';
+import { Container } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Title />
+        <Container className="content">
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" exact component={Profile} />
+          <Route path="/works" exact component={Works} />
+        </Container>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
