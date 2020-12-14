80dd5b9118374d829df8542f92a9788e8a5051a3
Reina Tochigi <tocchy@yutakanoMacBook-Pro.local>
2020-12-14 21:58:08 +0900

    プロフィールを作っている

--- src/App.js
+++ src/App.js
@@ -9,18 +9,17 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import Title from './components/Title';
 import Works from './components/Works';
-import { Container } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Title />
-        <Container className="content">
+        <div className="content">
           <Route path="/" exact component={Home} />
           <Route path="/profile" exact component={Profile} />
           <Route path="/works" exact component={Works} />
-        </Container>
+        </div>
       </BrowserRouter>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './style/Home.css'
 
 class Home extends React.Component {
     render() {
         return (
             <div className="home">
-                <h1>REINA TOCHIGI</h1>
+                <h1>REINA</h1>
+                <h1>TOCHIGI</h1>
             </div>
         )
     }
