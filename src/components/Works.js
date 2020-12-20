ea47b218431084edf076495b0202b7a5f2ace6a3
Reina Tochigi <tocchy@yutakanoMacBook-Pro.local>
2020-12-20 19:14:25 +0900

    worksに内容を追加

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import './style/Works.css'
 
 class Works extends React.Component {
     render() {
         return (
             < div className="works" >
                 <h1>Works</h1>
+                <div>
+                    <div className="product">
+                        <h2><a href="https://todot-com.herokuapp.com/" target="_blank">ToDotCom(トゥドットコム）</a>
+                        </h2>
+                        <p>Discordの勉強サーバーで「日課が続かない」という声を聞いて共感し、<br />
+                        日課を共有することで持続力がアップするのではないかと考えて
+                        日課共有専用のSNSを作ることにした。<br />
+                        バックエンドではGoを採用。こちらは奮わなかったインターンの内容を自学で習得するために選定した。</p>
+
+                    </div>
+                    <div className="product">
+                        <h2>
+                            <a href="#">猫が階段を登るCG</a>
+                        </h2>
+                        <p>Three.jsでCGを作れると聞いたので試しに作った。<br />
+                        猫が階段を登るところを工夫した。</p>
+                    </div>
+                    <div className="product">
+                        <h2>
+                            何か年内に作る
+                        </h2>
+                        <p></p>
+                    </div>
+                </div>
+
             </div >
         )
     }
