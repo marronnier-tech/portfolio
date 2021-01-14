179837cc7f06aa6ed847d34e2910f4a53c25a139
Reina Tochigi <tocchy@yutakanoMacBook-Pro.local>
2021-01-14 12:32:36 +0900

    CSSのimport

--- src/components/Abs.js
+++ src/components/Abs.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import './style/Profile.css'
+import './style/Abs.css'
 
 //var path;
 //path = props;
 //path = window.location.pathname.slice(1);
 
 class Abs extends React.Component {
-
     render() {
         return (
             <h1 class="top-item">{this.props.name}</h1>
